9f189ebf7412f839533df10116dedf1227b37480
Jorghee <jorgeehuarsaya@gmail.com>
2024-04-01 17:48:08 -0500

    Class that makes use of the fighter package, creating fighters randomly.

--- randomTroop/TroopFactory.java
+++ randomTroop/TroopFactory.java
@@ -1,16 +1,17 @@
 package randomTroop;
 
 import java.util.*;
-import randomTroop.warriors.*;
+import randomTroop.fighter.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 
 public class TroopFactory {
   private static boolean[][] position = new boolean[10][10];  
   private static final Random random = new Random();
 
-  public static List<Warrior> create(Image[] img) {
-    List<Warrior> troop = new LinkedList<>();
-    Warrior warrior = null;
+  public static List<Fighter<ImageView>> create(Image[] img) {
+    List<Fighter<ImageView>> troop = new LinkedList<>();
+    Fighter<ImageView> warrior = null;
 
     int total = random.nextInt(10) + 1;
     int count = 0;
@@ -23,23 +24,23 @@ public class TroopFactory {
       if(!position[x][y]) {
         switch(type) {
           case 0 :
-            warrior = new Lord("Lord " + count, x, y);
-            warrior.setSkin(img[0]);
+            warrior = new Lord<>("Lord " + count, x, y);
+            warrior.setSkin(new ImageView(img[0]));
             break;
 
           case 1 :
-            warrior = new Archer("Archer " + count, x, y);
-            warrior.setSkin(img[1]);
+            warrior = new Archer<>("Archer " + count, x, y);
+            warrior.setSkin(new ImageView(img[1]));
             break;
 
           case 2 :
-            warrior = new Swordsman("Swordsman " + count, x, y);
-            warrior.setSkin(img[2]);
+            warrior = new Swordsman<>("Swordsman " + count, x, y);
+            warrior.setSkin(new ImageView(img[2]));
             break;
 
           default:
-            warrior = new Lancer("Lancer " + count, x, y);
-            warrior.setSkin(img[3]);
+            warrior = new Lancer<>("Lancer " + count, x, y);
+            warrior.setSkin(new ImageView(img[3]));
             break;
         }
 
@@ -52,8 +53,8 @@ public class TroopFactory {
     return troop;
   }
 
-  public static Warrior getWarrior(List<Warrior> troop, int x, int y) {
-    for(Warrior i : troop)
+  public static Fighter<ImageView> getWarrior(List<Fighter<ImageView>> troop, int x, int y) {
+    for(Fighter<ImageView> i : troop)
       if(i.getPosition()[0] == x && i.getPosition()[1] == y)
         return i;
     return null;
