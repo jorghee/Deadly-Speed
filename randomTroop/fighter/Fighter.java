7878a1c077c98c07e3bb9bbfdf33884709c15d6a
Jorghee <jorgeehuarsaya@gmail.com>
2024-04-01 17:39:39 -0500

    This interface defines the behavior of all fighters. We abstract a Warrior and his skin will be an ImageView object (img).

---
+++ randomTroop/fighter/Fighter.java
@@ -0,0 +1,26 @@
+package randomTroop.fighter;
+
+public interface Fighter<T> {
+  void setName(String name);
+  void setHp(int hp);
+  void setAttack(int attack);
+  void setDefense(int defense);
+  void setSpeed(int speed);
+  void setPosition(int x, int y);
+  void setSkin(T skin);
+
+  String getName();
+  int getHp();
+  int getAttack();
+  int getDefense();
+  int getSpeed();
+  int[] getPosition();
+  T getSkin();
+
+  void moveTo(int x, int y);
+  boolean attackTo(int x, int y);
+  void gainSpeed(int x, int y);
+  boolean escapeTo(int x, int y);
+  String beAttackedBy(Fighter attacker);
+  double probabilityWinning(Fighter attacker);
+}
--- randomTroop/warriors/Lancer.java
+++
@@ -1,9 +0,0 @@
-package randomTroop.warriors;
-
-public class Lancer extends Warrior {
-  int spearLen;
-
-  public Lancer(String name, int x, int y) { 
-    super(name, random.nextInt(4) + 5, 5, 10, x, y);
-  }
-}
