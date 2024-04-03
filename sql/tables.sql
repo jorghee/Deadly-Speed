a79bdc30fbae932a5ebd476acaeddde6d2b7545d
Jorghee <jorgeehuarsaya@gmail.com>
2024-04-02 23:49:15 -0500

    Tables and fields ends

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,16 +1,21 @@
 CREATE TABLE Players (
-  player_id VARCHAR(255) PRIMARY KEY,
-  player_name VARCHAR(255) NOT NULL,
-  total_wins INT DEFAULT 0
+  id VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  salt VARCHAR(255) NOT NULL,
+  victories INT DEFAULT 0,
+  defeats INT DEFAULT 0
 );
 
 CREATE TABLE Games (
-  game_id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   player1_id VARCHAR(255),
   player2_id VARCHAR(255),
   winner_id VARCHAR(255), 
   game_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (player1_id) REFERENCES Players(player_id),
-  FOREIGN KEY (player2_id) REFERENCES Players(player_id),
-  FOREIGN KEY (winner_id) REFERENCES Players(player_id)
+  FOREIGN KEY (player1_id) REFERENCES Players(id),
+  FOREIGN KEY (player2_id) REFERENCES Players(id),
+  FOREIGN KEY (winner_id) REFERENCES Players(id)
 );
+
+CREATE INDEX idx_name ON Players (name);
