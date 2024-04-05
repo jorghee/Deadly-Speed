ac1423a71905109d8a4bdcbc183c6f80ceb19404
Jorghee <jorgeehuarsaya@gmail.com>
2024-04-04 23:42:26 -0500

    Abstracting to the Player, this object only lives in the champions league mode.

---
+++ connection/Player.java
@@ -0,0 +1,21 @@
+package connection;
+
+public class Player {
+  private String name;
+  private int id;
+  private StatePlayer statePlayer;
+
+  public Player(String name, int id, StatePlayer statePlayer) {
+    this.name = name;
+    this.id = id;
+    this.statePlayer = statePlayer;
+  }
+
+  public Player(StatePlayer statePlayer) { this.statePlayer = statePlayer; }
+
+  public String getName() { return name; }
+
+  public int getId() { return id; }
+
+  public StatePlayer getStatePlayer() { return statePlayer; }
+}
