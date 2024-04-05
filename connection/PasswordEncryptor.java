6d9e8800814a3ed7d96ce68ca4887044a4e3f617
Jorghee <jorgeehuarsaya@gmail.com>
2024-04-04 23:43:55 -0500

    Password security with the SHA-256 hash algorithm.

---
+++ connection/PasswordEncryptor.java
@@ -0,0 +1,27 @@
+package connection;
+
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class PasswordEncryptor {
+  public static String generateSalt() {
+    byte[] salt = new byte[16];
+    SecureRandom random = new SecureRandom();
+    random.nextBytes(salt);
+    return Base64.getEncoder().encodeToString(salt);
+  }
+
+  public static String getHashPassword(String password, String salt) {
+    try {
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      digest.update(Base64.getDecoder().decode(salt));
+
+      byte[] hashedBytes = digest.digest(password.getBytes());
+      return Base64.getEncoder().encodeToString(hashedBytes);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
