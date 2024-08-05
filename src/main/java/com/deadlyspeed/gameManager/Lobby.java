84145f64106bb1402b213cf9d8efdd2bc8ddd6f4
Jorghee <jorgeehuarsaya@gmail.com>
2024-08-05 11:02:35 -0500

    Ensured the creation of the scene by causing the mainLobby() method to be executed when necessary.

--- src/main/java/com/deadlyspeed/gameManager/Lobby.java
+++ src/main/java/com/deadlyspeed/gameManager/Lobby.java
@@ -7,19 +7,21 @@ import javafx.scene.Parent;
 import java.io.IOException;
 
 public class Lobby {
-
   private static Lobby instance;
-
   private Scene mainLobby;
 
-  public static Lobby getInstance() {
+  public static Lobby getInstance(Stage stage) {
     if(instance == null)
-      return new Lobby();
+      return new Lobby().mainLobby(stage);
 
     return instance;
   }
 
-  public void mainLobby(Stage stage) {
+  private Lobby() {
+    instance = this;
+  }
+
+  public Lobby mainLobby(Stage stage) {
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Lobby.fxml"));
     try {
       Parent root = loader.load();
@@ -28,6 +30,8 @@ public class Lobby {
     } catch(IOException e) {
       e.printStackTrace();
     }
+
+    return this;
   }
 
   public Scene getMainLobby() {
